952d39a54341f763dfb51480985c6507c0cc8573
HengkyLaurencio <liu.hengky.laurencio@gmail.com>
2024-05-01 20:35:13 +0700

    feat: add loginAttempts and lastLoginAttemp fields to usersSchema

--- src/models/users-schema.js
+++ src/models/users-schema.js
@@ -2,6 +2,8 @@ const usersSchema = {
   name: String,
   email: String,
   password: String,
+  loginAttempts: { type: Number, default: 0 },
+  lastLoginAttempt: { type: Date, default: null },
 };
 
 module.exports = usersSchema;
